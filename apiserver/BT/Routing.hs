40f53686c1dcd4c7a1b85064b71e028990b25da5
Colin Rice <colin@daedrum.net>
2013-01-28 09:01:22 -0500

    Update Skeleton to actually be used

--- apiserver/BT/Routing.hs
+++ apiserver/BT/Routing.hs
@@ -3,12 +3,15 @@ module BT.Routing where
 
 import Data.Map
 import BT.EndPoints
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy as BL
 
 router = Data.Map.fromList $ [
         ("", BT.EndPoints.helloworld),
         ("/", BT.EndPoints.helloworld),
         ("/balance", BT.EndPoints.helloworld)]
 
+route :: B.ByteString -> BL.ByteString
 route path = case Data.Map.lookup path router of
                 Nothing -> "404"
                 Just a -> a
--- apiserver/Main.hs
+++ apiserver/Main.hs
@@ -7,20 +7,17 @@ import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as C
 import BT.Routing
 
-getPathCheck :: B.ByteString -> Maybe B.ByteString
+getPathCheck :: B.ByteString -> B.ByteString
 getPathCheck path = let splitpath = C.split '/' path
                     in case length splitpath of
-                        0 -> Nothing
-                        1 -> Just ""
-                        _ -> Just (splitpath !! 1)
+                        0 -> ""
+                        1 -> ""
+                        _ -> (splitpath !! 1)
 
 application :: Application
 application info = do
     let path = rawPathInfo info
-        responsepath = case getPathCheck path of
-                            Nothing -> "Nope"
-                            Just a-> a
     return $
-        responseLBS status200 [("Content-Type", "text/plain")] (BL.fromChunks [responsepath, path])
+        responseLBS status200 [("Content-Type", "text/plain")] $ BT.Routing.route path
 
 main = run 3000 application

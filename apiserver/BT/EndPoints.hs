e7f81da3760a06e066c5792e0e156a55b6e02b2c
Colin Rice <colin@daedrum.net>
2013-01-30 19:14:01 -0500

    Make it much simpler to get random items

--- apiserver/BT/EndPoints.hs
+++ apiserver/BT/EndPoints.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
-module BT.EndPoints(register, ) where
+module BT.EndPoints(register) where
 import Data.ByteString.Lazy.Char8 (pack)
 import qualified Data.ByteString.Char8 as BC
 import qualified Data.ByteString as B
@@ -7,25 +7,16 @@ import qualified Data.ByteString.Lazy as BL
 import Database.Redis
 import Network.Wai (Request)
 import Numeric (showHex)
-import System.Random (getStdRandom, random)
 import Text.JSON
-import System.IO
 import BT.Global
-
-
-randomInt :: PersistentConns -> IO B.ByteString
-randomInt conns = do
-    fd <- openBinaryFile "/dev/urandom" ReadMode
-    randata <- hGetContents fd
-    hClose fd
-    return $ BC.pack $ take 256 randata
+import System.Entropy
 
 hex = foldr showHex "" . B.unpack
 
-register :: Request -> PersistentConns -> IO BL.ByteString
+register :: Request -> PersistentConns-> IO BL.ByteString
 register info conn = do
-    usernum <- randomInt conn
-    saltnum <- randomInt conn
+    usernum <- getEntropy 256
+    saltnum <- getEntropy 256
     let user = hex usernum
     let salt = hex usernum
     ok <- runRedis (redis conn) $ do
--- apiserver/BT/Global.hs
+++ apiserver/BT/Global.hs
@@ -1,15 +1,13 @@
-module BT.Global(PersistentConns, makeCons, randomfd, redis) where
+module BT.Global(PersistentConns, makeCons, redis) where
 
 import Database.Redis
 import System.IO
 
 data PersistentConns = PersistentConns {
-        randomfd :: Handle,
         redis :: Connection
         }
 
 makeCons :: IO PersistentConns
 makeCons = do
     conn <- connect defaultConnectInfo{connectPort = UnixSocket "/tmp/redis.sock", connectMaxConnections=500}
-    fd <- openBinaryFile "/dev/urandom" ReadMode
-    return PersistentConns{ randomfd=fd, redis=conn}
+    return PersistentConns{redis=conn}

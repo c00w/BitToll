6aad6fbac85308404843e8b090d6f83a854cab92
Colin Rice <colin@daedrum.net>
2013-02-02 22:54:15 -0500

    Actually talking to bitcoin server

--- bcserver/BCServer.hs
+++ bcserver/BCServer.hs
@@ -5,12 +5,15 @@ import Data.ByteString.Char8
 import qualified System.ZMQ3 as ZMQ
 import Control.Concurrent
 import Control.Monad
+import Network.Bitcoin as BTC
 
 main = do
     let bindTo = "tcp://*:3333"
+    let bcd = BTC.Auth "http://127.0.0.1:8332" "FGHJUYTUJKNMBVCCDFSTRdfyhydsaoiuyaustdyutyoiurewri" "jakhdkjahslkjdhlkjfhdskjlhflkjHJITYUIOTRRRRYII"
     ZMQ.withContext $ \c ->
         ZMQ.withSocket c ZMQ.Rep $ \s -> do
             ZMQ.bind s bindTo
             forever $ do
                 request <- ZMQ.receive s
-                ZMQ.send s [] "PONG"
+                diff <- BTC.getDifficulty bcd
+                ZMQ.send s [] $ pack $ show diff

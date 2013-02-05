301cf0bede26ac2ece5fce12fc2109d7c100fd5a
Colin Rice <colin@daedrum.net>
2013-02-05 10:39:28 -0500

    Catch errors and print them

--- apiserver/APIServer.hs
+++ apiserver/APIServer.hs
@@ -2,25 +2,32 @@
 import Network.Wai
 import Network.Wai.Handler.Warp (run)
 import Network.HTTP.Types (status200)
-import qualified Data.ByteString.Lazy as BL
-import qualified Data.ByteString as B
+import Prelude hiding (pack, split)
+import Data.ByteString (ByteString, pack)
 import Data.ByteString.Char8 (split)
+import qualified Data.ByteString.Lazy as LB
 import Control.Monad.IO.Class (liftIO)
 import BT.Routing
 import BT.Global
-import Database.Redis 
+import Control.Exception (catch, SomeException)
+import System.IO(hPutStr, stderr)
 
-getPathCheck :: B.ByteString -> B.ByteString
+getPathCheck :: ByteString -> ByteString
 getPathCheck path = let splitpath = split '/' path
                     in case length splitpath of
                         0 -> ""
                         1 -> ""
                         _ -> (splitpath !! 1)
 
+exceptionHandler :: SomeException -> IO LB.ByteString
+exceptionHandler e = do
+    hPutStr stderr $ show e
+    return "Server Error"
+
 application :: PersistentConns -> Application
 application conns info = do
     let path = rawPathInfo info
-    response <- liftIO $ BT.Routing.route path info conns
+    response <- liftIO $ catch ( BT.Routing.route path info conns ) exceptionHandler
     return $
         responseLBS status200 [("Content-Type", "text/plain")] response
 

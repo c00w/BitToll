be920db93db2474d5166f4a65ddc07700bbe6a52
Colin Rice <colin@daedrum.net>
2013-02-15 23:31:39 -0500

    Added basic py.test infrastructure

---
+++ testsuite/front_test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python2
+import gevent
+import requests
+import json
+
+ip_address = "192.168.56.101"
+port = "3000"
+url = ''.join(['http://', ip_address, ':', port])
+
+def test_login():
+    r = requests.get(url + '/register')
+    assert r.status_code == 200
+    info = json.loads(r.text)
+    assert 'username' in info
+    assert 'secret' in info
+    assert (info.keys()) == 2
+

33d6e0c259f4bff33e3ee107fa47f98e628fb07b
Colin Rice <colin@daedrum.net>
2013-03-15 22:22:09 -0400

    Updated puppet configuration, everything works

--- puppet/test_vm.pp
+++ puppet/test_vm.pp
@@ -9,12 +9,17 @@ $test_packages = [
 
 package { $test_packages:
     ensure  =>  latest,
+    require =>  Exec["apt_update"]
 }
 
-exec { "apt-add-repository ppa:bitcoin/bitcoin":
-    alias   =>  "ppa_bitcoin",
+exec {"/usr/bin/apt-get update":
+    alias => "apt_update"
+}
+
+exec { "/usr/bin/apt-add-repository ppa:bitcoin/bitcoin || /usr/bin/apt-get update":
+    alias   => "ppa_bitcoin",
     require => Package["python-software-properties"],
-    creates => "/etc/apt/sources.list.d/bitcoin-bitcoin-precise.list"
+    creates => "/etc/apt/sources.list.d/bitcoin-bitcoin-precise.list",
 }
 
 package {"bitcoind":
@@ -23,14 +28,47 @@ package {"bitcoind":
 }
 
 service {"redis-server":
-    requires => Package["redis-server"],
+    require => Package["redis-server"],
     ensure => running,
     enable => true,
 }
 
 user {"bitcoind":
     ensure => present,
-    managehome => present,
+    home => "/home/bitcoind",
+    managehome => true,
 }
 
+file {"/home/bitcoind/.bitcoin":
+    require => User["bitcoind"],
+    alias => "bitcoin_folder",
+    ensure => directory,
+    mode => 0644,
+    owner => "bitcoind",
+}
 
+file {"/home/bitcoind/.bitcoin/bitcoin.conf":
+    require => File["bitcoin_folder"],
+    ensure => present,
+    mode => 0644,
+    source => "/configs/bitcoin.conf",
+    owner => "bitcoind",
+    alias => "bitcoin.conf"
+}
+
+file {"/etc/init/bitcoind.conf":
+    ensure => present,
+    mode => 0644,
+    source => "/configs/bitcoind.conf",
+    alias => "bitcoind.conf"
+}
+
+service {"bitcoind":
+    require => [
+        Package["bitcoind"],
+        File["bitcoin.conf"],
+        File["bitcoind.conf"],
+    ],
+    ensure => running,
+    enable => true,
+}

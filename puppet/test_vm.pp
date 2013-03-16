252dd0beebce2aac13be1bc0b714360b68490550
Colin Rice <colin@daedrum.net>
2013-03-15 23:36:04 -0400

    Haskell binaries now deploy correctly

--- puppet/test_vm.pp
+++ puppet/test_vm.pp
@@ -1,9 +1,6 @@
 
 $test_packages = [
-    "htop",
-    "zsh",
     "redis-server",
-    "vim",
     "python-software-properties",
 ]
 
@@ -16,15 +13,28 @@ exec {"/usr/bin/apt-get update":
     alias => "apt_update"
 }
 
-exec { "/usr/bin/apt-add-repository ppa:bitcoin/bitcoin || /usr/bin/apt-get update":
+exec { "/usr/bin/apt-add-repository ppa:bitcoin/bitcoin && /usr/bin/apt-get update":
     alias   => "ppa_bitcoin",
     require => Package["python-software-properties"],
     creates => "/etc/apt/sources.list.d/bitcoin-bitcoin-precise.list",
 }
 
+exec { "/usr/bin/apt-add-repository ppa:chris-lea/zeromq && /usr/bin/apt-get update":
+    alias   => "ppa_zeromq",
+    require => Package["python-software-properties"],
+    creates => "/etc/apt/sources.list.d/chris-lea-zeromq-precise.list",
+}
+
+
 package {"bitcoind":
     require => Exec["ppa_bitcoin"],
     ensure => latest,
+}
+
+package {"libzmq1":
+    require => Exec["ppa_zeromq"],
+    ensure => latest,
+    alias => "zeromq",
 }
 
 service {"redis-server":
@@ -35,10 +45,20 @@ service {"redis-server":
     hasrestart => true,
 }
 
-user {"bitcoind":
+User {
     ensure => present,
-    home => "/home/bitcoind",
     managehome => true,
+}
+
+user {
+    "bitcoind":
+        home => "/home/bitcoind";
+    "apiserver":
+        home => "/home/apiserver";
+    "bcserver":
+        home => "/home/bcserver";
+    "poolserver":
+        home => "/home/poolserver";
 }
 
 file {"/home/bitcoind/.bitcoin":
@@ -74,3 +94,75 @@ service {"bitcoind":
     ensure => running,
     enable => true,
 }
+
+file {"/usr/bin/BCServer":
+    ensure => present,
+    mode => 0777,
+    source => "/binaries/BCServer/BCServer",
+    alias => "bc-binary",
+}
+
+file {"/usr/bin/APIServer":
+    ensure => present,
+    mode => 0777,
+    source => "/binaries/APIServer/APIServer",
+    alias => "api-binary",
+}
+
+file {"/usr/bin/PoolServer":
+    ensure => present,
+    mode => 0777,
+    source => "/binaries/PoolServer/PoolServer",
+    alias => "pool-binary",
+}
+
+file {"/etc/init/apiserver.conf":
+    ensure => present,
+    mode => 0644,
+    source => "/configs/apiserver.conf",
+    alias => "apiserver.conf"
+}
+
+file {"/etc/init/bcserver.conf":
+    ensure => present,
+    mode => 0644,
+    source => "/configs/bcserver.conf",
+    alias => "bcserver.conf"
+}
+
+file {"/etc/init/poolserver.conf":
+    ensure => present,
+    mode => 0644,
+    source => "/configs/poolserver.conf",
+    alias => "poolserver.conf"
+}
+
+Service {
+    ensure => running,
+    enable => true,
+}
+
+service {"apiserver":
+    require => [
+        User["apiserver"],
+        Package["zeromq"],
+        File["apiserver.conf"],
+    ],
+}
+
+service {"bcserver":
+    require => [
+        User["bcserver"],
+        Package["zeromq"],
+        File["bcserver.conf"],
+    ],
+}
+
+service {"poolserver":
+    require => [
+        User["poolserver"],
+        Package["zeromq"],
+        File["poolserver.conf"],
+    ],
+}
+

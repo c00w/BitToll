59287b31bf3c88e690f55183f990a2ec22acc249
Colin Rice <colin@daedrum.net>
2013-05-05 19:19:55 -0400

    Add actual accesors

--- haskell/BT/Mining.hs
+++ haskell/BT/Mining.hs
@@ -6,10 +6,12 @@ import qualified Data.ByteString.Char8 as BC
 import Prelude hiding (take, drop)
 import Data.Text.Encoding as E
 import BT.Types
-import BT.Redis (get, set)
+import BT.Redis
+import BT.Util
 import Numeric (readHex)
 import Network.Bitcoin (BTC)
 import Data.IORef (readIORef)
+import Control.Monad (when, liftM)
 
 storeMerkleDiff :: PersistentConns -> HashData -> IO ()
 storeMerkleDiff conn hashData = do
@@ -43,14 +45,35 @@ getPayout conn hexdiff = do
     let diff = (hexDiffToInt hexdiff) :: BTC
     return $ (miningDiff * payout) / diff
 
+setShareUsername :: PersistentConns -> B.ByteString -> B.ByteString -> IO Bool
+setShareUsername conn shareid username = setnx conn "s:" shareid "username" username
+
+setSharePayout :: PersistentConns -> B.ByteString -> BTC -> IO Bool
+setSharePayout conn shareid payout = setbtc conn "s:" shareid "payout" payout
+
+setSharePercentPaid :: PersistentConns -> B.ByteString -> BTC -> IO Bool
+setSharePercentPaid conn shareid payout = set conn "s:" shareid "percentpaid" (BC.pack . show $ payout)
+
 --- Make a share object containing a payout, percent paid, username
 makeShare :: PersistentConns -> B.ByteString -> IO B.ByteString
 makeShare conn username = do
-    shareid <- random256String
-    resp <- setnx conn "s:" shareid "username" username of
+    shareid <- (liftM BC.pack) random256String
+    resp <- setShareUsername conn shareid username
     when (resp == True) $ do
-        setbtc conn "s:" shareid "payout" 0
-        set conn "s:" shareid "percentpaid" "0.0"
+        _ <- setSharePayout conn shareid 0
+        _ <- setSharePercentPaid conn shareid 0.0
+        return ()
     case resp of
         True -> return shareid
-        False -> return makeShare conn username
+        False -> makeShare conn username
+
+--- Structure
+--- username set of all share keys sorted by payout
+--- global sorted set of all share keys sorted by payout
+
+
+
+--- Get the current mining share
+--- getCurrentMiningShare :: PersistentConns -> B.ByteString -> IO B.ByteString
+--- getCurrentMiningShare conn username = do
+---     zrangebyscore "username"

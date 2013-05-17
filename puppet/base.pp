3a4645e4345dd4261b8b6954d3005d375cce7b84
Colin Rice <colin@daedrum.net>
2013-05-16 23:24:19 -0400

    Do unattended upgrades

--- puppet/base.pp
+++ puppet/base.pp
@@ -1,6 +1,10 @@
 
+Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
+
 node default {
     class {'ntp':}
+    class {'apt':}
+    class {'apt::unattended-upgrade::automatic':}
     import "test_vm.pp"
 }
 

e5e1313c8982cc86f74aad1409e20aeb49d9bee7
Colin Rice <colin@daedrum.net>
2013-05-20 14:06:44 -0400

    Do a recursive checkout and quote correctly

--- production/bootstrap.sh
+++ production/bootstrap.sh
@@ -4,5 +4,5 @@ set -e
 
 ssh root@$1 "apt-get -y install git
 rm -r ~/*
-git clone git://github.com/c00w/BitToll.git -b production
+git clone git://github.com/c00w/BitToll.git -b production --recursive
 ~/BitToll/production/local_bootstrap.sh"
--- production/puppet_run.sh
+++ production/puppet_run.sh
@@ -2,4 +2,4 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-puppet apply  --modulepath '$DIR/../puppet_modules' $DIR/../puppet/base.pp --detailed-exitcodes || [ $? -eq 2 ]
+puppet apply  --modulepath "$DIR/../puppet_modules" $DIR/../puppet/base.pp --detailed-exitcodes || [ $? -eq 2 ]

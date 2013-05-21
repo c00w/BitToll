07f3397910f23919dfcb9edc0dfb2a25050ccaee
Colin Rice <colin@daedrum.net>
2013-05-21 17:04:28 -0400

    Run puppet as root

--- dev/ubuntu_setup.sh
+++ dev/ubuntu_setup.sh
@@ -4,4 +4,13 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 sudo $DIR/../production/setup_puppet.sh
-FACTER_curruser=$(whoami) puppet apply  --modulepath "$DIR/../puppet_modules" $DIR/../puppet/dev.pp --detailed-exitcodes || [ $? -eq 2 ]
+sudo FACTER_curruser=$(whoami) puppet apply  --modulepath "$DIR/../puppet_modules" $DIR/../puppet/dev.pp --detailed-exitcodes || [ $? -eq 2 ]
+
+wget http://files.vagrantup.com/packages/7e400d00a3c5a0fdf2809c8b5001a035415a607b/vagrant_1.2.2_x86_64.deb
+sudo dpkg -i vagrant_1.2.2_x86_64.deb
+
+sudo apt-get install python-pip python-requests
+sudo pip install fabric py-test
+
+echo "192.168.56.2    vm  " >> /etc/hosts
+

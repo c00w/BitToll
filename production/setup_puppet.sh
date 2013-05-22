75d936089c9c7618df42eba3bd5cdfc1b8430957
Colin Rice <colin@daedrum.net>
2013-05-22 17:54:54 -0400

    No prompt for install of puppet

--- production/setup_puppet.sh
+++ production/setup_puppet.sh
@@ -8,4 +8,4 @@ dpkg -i puppetlabs-release-precise.deb
 rm puppetlabs-release-precise.deb
 apt-key adv --recv-key --keyserver pool.sks-keyservers.net 4BD6EC30
 apt-get update
-apt-get install puppet
+apt-get install puppet -y

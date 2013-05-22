692e965eddf2ec9d804ef23a867f3825ad1b2386
Colin Rice <colin@daedrum.net>
2013-05-22 12:06:36 -0400

    Actually needed that exec line after all

--- puppet/base.pp
+++ puppet/base.pp
@@ -1,11 +1,13 @@
 
+Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
+
 import "build.pp"
 
 class {'ntp':}
 class {'apt':}
 class {'apt::unattended-upgrade::automatic':}
 
-node atlantis.m.bittoll.com {
+node "atlantis.m.bittoll.com" {
     class {'build_depends':}
     import "test_vm.pp"
 }

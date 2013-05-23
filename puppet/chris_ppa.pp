b848d67463c8609e2ee92235cd4d71b7863f6b15
Colin Rice <colin@daedrum.net>
2013-05-22 20:52:00 -0400

    Found the bug, forgot to order exec update

--- puppet/chris_ppa.pp
+++ puppet/chris_ppa.pp
@@ -1,11 +1,12 @@
 class chris_ppa {
-    apt::ppa {'chris-lea':
+    apt::ppa {"chris-lea":
         ensure  => present,
         key     => "C7917B12",
         ppa     => ["redis-server", "zeromq"]
     }
 
     exec {"apt-get update && touch /var/tmp/chris_ppa_update":
+        require => Apt::Ppa["chris-lea"],
         path    => "/usr/bin",
         creates => "/var/tmp/chris_ppa_update",
     }

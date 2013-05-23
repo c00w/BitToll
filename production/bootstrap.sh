c3db5462d73707d12bfa482fb31703dc71c4ec49
Colin Rice <colin@daedrum.net>
2013-05-22 20:52:09 -0400

    Clean root after bootstrap

--- production/bootstrap.sh
+++ production/bootstrap.sh
@@ -9,8 +9,8 @@ echo \"127.0.0.1    localhost\" > /etc/hosts
 echo \"127.0.1.1    $2.m.bittoll.com $2\" >> /etc/hosts
 echo \"$1   $2.m.bittoll.com $2\" >> /etc/hosts
 apt-get -y install git
-rm -r ~/*
 git clone git://github.com/c00w/BitToll.git -b production --recursive
 ~/BitToll/production/setup_puppet.sh
 ~/BitToll/production/run_puppet.sh
+rm -r ~/*
 "

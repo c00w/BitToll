63ca74d742b35fe302314021ac8e59761540656f
Colin Rice <colin@daedrum.net>
2013-05-27 09:37:32 -0400

    use aeson instead of json for encoding

--- haskell/BT/Routing.hs
+++ haskell/BT/Routing.hs
@@ -5,26 +5,25 @@ import Data.Map
 import BT.EndPoints
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as BL
-import Data.ByteString.Lazy.Char8 (pack)
 import Network.Wai (Request)
 import BT.Types (PersistentConns)
-import Text.JSON (toJSObject, encode)
+import Data.Aeson.Encode (encode)
 
 jstostring :: (Request -> PersistentConns -> IO [(String, String)]) -> Request -> PersistentConns -> IO BL.ByteString
 jstostring obj r p= do
     resp <- obj r p
-    return . pack . encode . toJSObject $ resp
+    return . encode . Data.Map.fromList $ resp
 
 router :: Map B.ByteString (Request -> PersistentConns -> IO BL.ByteString)
 router = Data.Map.fromList $ [
-        ("/register", jstostring BT.EndPoints.register),
-        ("/balance", jstostring BT.EndPoints.getBalance),
-        ("/withdraw", jstostring BT.EndPoints.sendBTC),
-        ("/deposit", jstostring BT.EndPoints.deposit),
-        ("/request", jstostring BT.EndPoints.createPayment),
-        ("/mine", BT.EndPoints.mine),
-        ("/", BT.EndPoints.mine),
-        ("/pay", jstostring BT.EndPoints.makePayment)]
+        ("/register",   jstostring BT.EndPoints.register),
+        ("/balance",    jstostring BT.EndPoints.getBalance),
+        ("/withdraw",   jstostring BT.EndPoints.sendBTC),
+        ("/deposit",    jstostring BT.EndPoints.deposit),
+        ("/request",    jstostring BT.EndPoints.createPayment),
+        ("/mine",       BT.EndPoints.mine),
+        ("/",           BT.EndPoints.mine),
+        ("/pay",        jstostring BT.EndPoints.makePayment)]
 
 route :: B.ByteString -> Request -> PersistentConns -> IO BL.ByteString
 route path info conns = case Data.Map.lookup path router of

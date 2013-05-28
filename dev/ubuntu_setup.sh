dbd613bde51ee5de68bd52113174c0c4af087d8f
Colin Rice <colin@daedrum.net>
2013-05-28 13:00:15 -0400

    Remove puppet setup

--- dev/ubuntu_setup.sh
+++ dev/ubuntu_setup.sh
@@ -2,9 +2,10 @@
 
 set -e
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-sudo $DIR/../production/setup_puppet.sh
-sudo FACTER_curruser=$(whoami) puppet apply  --modulepath "$DIR/../puppet_modules" $DIR/../puppet/dev.pp --detailed-exitcodes || [ $? -eq 2 ]
+# Not working
+#DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+#sudo $DIR/../production/setup_puppet.sh
+#sudo FACTER_curruser=$(whoami) puppet apply  --modulepath "$DIR/../puppet_modules" $DIR/../puppet/dev.pp --detailed-exitcodes || [ $? -eq 2 ]
 
 wget http://files.vagrantup.com/packages/7e400d00a3c5a0fdf2809c8b5001a035415a607b/vagrant_1.2.2_x86_64.deb
 sudo dpkg -i vagrant_1.2.2_x86_64.deb

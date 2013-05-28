04e93c288eaa5aadb09f98649d1f59005325f659
Colin Rice <colin@daedrum.net>
2013-05-28 16:43:20 -0400

    Use wget -C

--- dev/ubuntu_setup.sh
+++ dev/ubuntu_setup.sh
@@ -6,7 +6,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 sudo $DIR/../production/setup_puppet.sh
 sudo FACTER_curruser=$(whoami) puppet apply  --modulepath "$DIR/../puppet_modules" $DIR/../puppet/dev.pp --detailed-exitcodes || [ $? -eq 2 ]
 
-wget http://files.vagrantup.com/packages/7e400d00a3c5a0fdf2809c8b5001a035415a607b/vagrant_1.2.2_x86_64.deb
+wget http://files.vagrantup.com/packages/7e400d00a3c5a0fdf2809c8b5001a035415a607b/vagrant_1.2.2_x86_64.deb -C
 sudo dpkg -i vagrant_1.2.2_x86_64.deb
 rm vagrant_1.2.2_x86_64.deb
 

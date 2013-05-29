5c49e74e04712de6b17a5c2a845c6281acb5ac92
Colin Rice <colin@daedrum.net>
2013-05-29 13:49:13 -0400

    Better logging in timeout case

--- haskell/APIServer.hs
+++ haskell/APIServer.hs
@@ -30,11 +30,13 @@ application :: PersistentConns -> Application
 application conns info = do
     let path = rawPathInfo info
     responsew <- liftIO $ catch (timeout 30000000 ( BT.Routing.route path info conns )) exceptionHandler
-    liftIO $ hPutStrLn stdout $ show responsew
+    liftIO $ PutStrLn $ show responsew
     case responsew of
         Just response -> return $
             responseLBS status200 [("Content-Type", "text/plain")] response
-        Nothing -> return $ responseLBS status200 [] "{\"error\":\"Server Error\"}"
+        Nothing -> do
+            liftIO $ PutStrLn "Api call timed out"
+            return $ responseLBS status200 [] "{\"error\":\"Server Error\"}"
 
 main :: IO ()
 main = do

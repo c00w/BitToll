67ce0fe3c6325ff5b22c72fc05e28ccafa0e2c85
Colin Rice <colin@daedrum.net>
2013-05-29 17:56:07 -0400

    Fix whitespace

--- haskell/BT/Util.hs
+++ haskell/BT/Util.hs
@@ -15,21 +15,21 @@ import Network.Wai (Request, requestBody)
 import Data.Monoid (mconcat)
 import Data.String (IsString)
 
-randomNum :: IO Word64 
-randomNum = randomIO 
+randomNum :: IO Word64
+randomNum = randomIO
 
-randomString :: IO String 
-randomString = do 
-    a <- randomNum 
-    return $ showHex a "" 
+randomString :: IO String
+randomString = do
+    a <- randomNum
+    return $ showHex a ""
 
-random256String :: IO String 
-random256String = do 
-    a <- randomString 
-    b <- randomString 
-    c <- randomString 
-    d <- randomString 
-    return $ a ++ b ++ c ++ d 
+random256String :: IO String
+random256String = do
+    a <- randomString
+    b <- randomString
+    c <- randomString
+    d <- randomString
+    return $ a ++ b ++ c ++ d
 
 getResult :: Result a -> a
 getResult res = case res of

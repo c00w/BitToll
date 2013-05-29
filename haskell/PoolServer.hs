c418fba70d648bbd6ebb3f624e7a132c1135774b
Colin Rice <colin@daedrum.net>
2013-05-29 16:20:38 -0400

    Make poolserver take a configuration file

--- haskell/PoolServer.hs
+++ haskell/PoolServer.hs
@@ -8,31 +8,41 @@ import qualified System.ZMQ3 as ZMQ
 import Control.Monad
 import Network.Bitcoin as BTC
 import Data.Text.Encoding (decodeUtf8)
+import Data.Text (pack)
 
 import Data.Aeson
 
-bcdmine :: Auth
-bcdmine = BTC.Auth "http://127.0.0.1:19332" "x" "x"
+import BT.Config
+
+makebcd :: IO Auth
+makebcd = do
+    config <- makeConfig
+    host <- getConfig config "p2pool.host" :: IO String
+    port <- getConfig config "p2pool.port" :: IO Int
+    let url = pack $ "http://" ++ host ++ ":" ++ (show port)
+    return $ BTC.Auth url "x" "x"
 
 main :: IO ()
 main = do
+    bcd <- makebcd
+    let routes = makeRouter bcd
     let bindTo = "tcp://*:4444"
     ZMQ.withContext $ \c ->
         ZMQ.withSocket c ZMQ.Rep $ \s -> do
             ZMQ.bind s bindTo
             forever $ do
                 request <- ZMQ.receive s
-                resp <- route request
+                resp <- route routes request
                 ZMQ.send s [] $ resp
 
-router :: Map ByteString (ByteString -> IO ByteString)
-router = Data.Map.fromList $ [
+makeRouter :: Auth -> Map ByteString (ByteString -> IO ByteString)
+makeRouter bcdmine = Data.Map.fromList $ [
     ("getwork", getwork bcdmine),
     ("recvwork", recvwork bcdmine)
     ]
 
-route :: ByteString -> IO ByteString
-route request = case Data.Map.lookup (take 7 request) router of
+route :: (Map ByteString (ByteString -> IO ByteString)) -> ByteString -> IO ByteString
+route router request = case Data.Map.lookup (take 7 request) router of
     Just a -> a (drop 7 request)
     Nothing -> case Data.Map.lookup (take 8 request) router of
         Just a -> a (drop 8 request)

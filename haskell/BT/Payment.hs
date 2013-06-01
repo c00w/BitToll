850322145563d527a1fed76df83a02e14de7e94a
Colin Rice <colin@daedrum.net>
2013-05-31 23:52:15 -0400

    Fix payment.hs

--- haskell/BT/Payment.hs
+++ haskell/BT/Payment.hs
@@ -12,18 +12,18 @@ getPaymentAmount conn paymentid = getbtc conn "p:" paymentid "amount"
 getPaymentUser :: PersistentConns -> B.ByteString -> IO (Maybe B.ByteString)
 getPaymentUser conn paymentid = get conn "p:" paymentid "user"
 
-getPaymentDone :: PersistentConns -> B.ByteString -> IO (Bool)
+getPaymentDone :: PersistentConns -> B.ByteString -> IO Bool
 getPaymentDone conn paymentid = do
     r <- get conn "p:" paymentid "done"
     case r of
         Nothing -> return False
         Just _ -> return True
 
-setPaymentAmount :: PersistentConns -> B.ByteString -> BTC -> IO (Bool)
-setPaymentAmount conn paymentid value = setnxbtc conn "p:" paymentid "amount" value
+setPaymentAmount :: PersistentConns -> B.ByteString -> BTC -> IO Bool
+setPaymentAmount conn paymentid = setnxbtc conn "p:" paymentid "amount"
 
-setPaymentUser :: PersistentConns -> B.ByteString -> B.ByteString -> IO (Bool)
-setPaymentUser conn paymentid value = setnx conn "p:" paymentid "user" value
+setPaymentUser :: PersistentConns -> B.ByteString -> B.ByteString -> IO Bool
+setPaymentUser conn paymentid = setnx conn "p:" paymentid "user"
 
-setPaymentDone :: PersistentConns -> B.ByteString -> IO (Bool)
+setPaymentDone :: PersistentConns -> B.ByteString -> IO Bool
 setPaymentDone conn paymentid = setnx conn "p:" paymentid "done" "1"

5fecbe593caa2288168898a6db82c0b5a3e0fb60
Colin Rice <colin@daedrum.net>
2013-06-02 17:41:25 -0400

    Add lua as a proxy layer for merkle root logging

--- puppet/nginx.pp
+++ puppet/nginx.pp
@@ -7,7 +7,7 @@ class nginx ($test = false) {
         ppa     => "stable",
     }
 
-    package {"nginx":
+    package {"nginx-openresty":
         ensure  => latest,
         require  => Apt::Ppa["nginx-openresty"],
     }
@@ -19,19 +19,19 @@ class nginx ($test = false) {
 
     file {"/etc/nginx/sites-available/default":
         notify  => Service["nginx"],
-        require => Package["nginx"],
+        require => Package["nginx-openresty"],
         ensure  => absent,
     }
 
     file {"/etc/nginx/sites-enabled/default":
         notify  => Service["nginx"],
-        require => Package["nginx"],
+        require => Package["nginx-openresty"],
         ensure  => absent
     }
 
     file {"/etc/nginx/sites-available/bittoll.conf":
         notify  => Service["nginx"],
-        require => Package["nginx"],
+        require => Package["nginx-openresty"],
         ensure  => present,
         mode    => 0400,
         source  => "$config/nginx/bittoll.conf",
@@ -39,21 +39,21 @@ class nginx ($test = false) {
 
     file {"/etc/nginx/sites-enabled/bittoll.conf":
         notify  => Service["nginx"],
-        require => Package["nginx"],
+        require => Package["nginx-openresty"],
         ensure  => link,
         target  => "/etc/nginx/sites-available/bittoll.conf",
     }
 
     file {"/etc/nginx/nginx.conf":
         notify  => Service["nginx"],
-        require => Package["nginx"],
+        require => Package["nginx-openresty"],
         ensure  => file,
         mode    => 0400,
         source  => "$config/nginx.conf",
     }
 
     service {"nginx":
-        require => Package["nginx"],
+        require => Package["nginx-openresty"],
         ensure  => running,
         enable  => true,
         hasrestart  => true,

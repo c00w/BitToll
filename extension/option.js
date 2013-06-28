3c85d9015b1696700da64d8fa951cfb0b7794ba3
Colin Rice <colin@daedrum.net>
2013-06-28 18:38:45 -0300

    Angular js now works and the register talks to bittoll

---
+++ extension/option.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/* Controllers */
+
+
+
+function LoginCtrl($scope, $http) {
+    $scope.hi = "hello world"
+    $scope.tryLogin = function (username, password) {
+        console.log(username)
+        console.log(password)
+    }
+    $scope.register = function (username, password) {
+        $http.post("https://us.bittoll.com/register","{}").
+        success(function(data, status, headers, config) {
+            var login = data
+            delete(login["error_code"])
+            console.log("succ")
+            console.log(login)
+        }).
+        error(function(data, status, headers, config) {
+            console.log("error")
+            console.log(data)
+        })
+        console.log("register")
+        console.log(username)
+        console.log(password)
+    }
+}
+

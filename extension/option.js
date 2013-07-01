eccc9844edd998ebc7670af14be2edbe24b1b531
Colin Rice <colin@daedrum.net>
2013-07-01 13:28:22 -0300

    Login and register work!!!

--- extension/option.js
+++ extension/option.js
@@ -7,12 +7,39 @@
 function LoginCtrl($scope, $http) {
     $scope.hi = "hello world"
     $scope.tryLogin = function (username, password) {
+        $http.post(
+            "https://us.bittoll.com/alias",
+            {
+                "aliasName": username,
+                "aliasPassword": password,
+            }
+        ).
+        success(function(data, status, haders, config) {
+            if (data["error_code"] !== "0") {
+                console.log("error")
+                console.log(data)
+                return
+            }
+            delete(data["error_code"])
+            console.log("success login")
+            $scope.login = data
+        }).
+        error(function(data, status, headers, config) {
+            console.log("error")
+            console.log("data")
+        })
+
         console.log(username)
         console.log(password)
     }
     $scope.register = function (username, password) {
         $http.post("https://us.bittoll.com/register","{}").
         success(function(data, status, headers, config) {
+            if (data["error_code"] !== "0") {
+                console.log("error")
+                console.log(data)
+                return
+            }
             var login = data
             delete(login["error_code"])
             console.log("succ")
@@ -28,6 +55,11 @@ function LoginCtrl($scope, $http) {
                 )
             ).
             success(function(data, status, headers, config) {
+                if (data["error_code"] !== "0") {
+                    $scope.login = undefined
+                    console.log("succ err")
+                    return
+                }
                 console.log(data)
                 $scope.login = login
                 console.log("succ al")

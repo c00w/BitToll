1882d3b2086a5c1925589d6cfdf24ed5791bc1b9
Colin Rice <colin@daedrum.net>
2013-07-03 14:10:01 -0300

    Remove my silly button stuff

--- extension/js/background.js
+++ extension/js/background.js
@@ -1,24 +1,9 @@
 
-function show(tab) {
-    chrome.pageAction.show(tab.id);
-    console.log(tab.id);
-    chrome.notifications.create("b", opt, null);
-};
-
-function showid(tabid) {
-    chrome.pageAction.show(tabid);
-    console.log(tabid)
-    chrome.notifications.create("b", opt, null);
-};
-
 var opt = {
     type: "basic",
     title: "bob",
     message: "bob"
 }
-
-chrome.tabs.onCreated.addListener(show);
-
 
 function passMessageToContentScript( payment_return){
 }

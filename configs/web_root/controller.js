29b2c665936241814789aa045fa80fce032a248b
Colin Rice <colin@daedrum.net>
2013-07-11 17:08:14 -0400

    Balanced can't deal with us, apparently dwolla can with approval

--- configs/web_root/controller.js
+++ configs/web_root/controller.js
@@ -1,24 +1,7 @@
 
 function CCPaymentCtrl($scope, $http) {
-    balanced.init('${REPLACE_THIS_WITH_YOUR_MARKETPLACE_URI}');
 
     //Process Info to get URI, send to server
     $scope.pay = function (number, month, year, code) {
-        console.log(number)
-        console.log(month)
-        console.log(year)
-        console.log(code)
-        var creditCardData = {
-            card_number: number,
-            expiration_month: month,
-            expiration_year: year,
-            security_code: code
-        }
-        balanced.card.create(creditCardData, function(response) {
-            console.log(response.status)
-            console.log(response.data)
-            console.log(response.error)
-        })
-
     }
 }
